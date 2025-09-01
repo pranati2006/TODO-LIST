e040062e41f18e84522a30b0bee7f239cbf4d67f
pranati2006 <pranatisr06@gmail.com>
2025-09-01 13:36:07 +0000

    setup

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,18 @@
 
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MainScreen from './mainscreen.js';
+import AddTasks from './addtasks.js';
+import DisplayTasks from './displayTasks.js';
 
 function App() {
-  return (
-    <div className="App">
-      HI
-    </div>
-  );
-}
+  <Router>
+      <Routes>
+        <Route path="/" element={<MainScreen />} />
+        <Route path="addtask" element={<AddTasks />} />
+        <Route path="displaytask" element={<DisplayTasks/>} />
+      </Routes>
+  </Router>
+  
+};
 
 export default App;
---
+++ my-react-app/src/addtasks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddTasks() {
+    return (
+        <div >
+
+
+        </div>
+    );
+}
+
+export default AddTasks;
